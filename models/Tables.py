1e70bf6b5c471b06a5df484e46ab558540a01b20
vidishraj <vidishraj@gmail.com>
2025-01-09 22:14:35 +0530

    feat: Added models for tables and init sqlalchemy

---
+++ models/Tables.py
@@ -0,0 +1,73 @@
+from datetime import datetime
+from models.Base import Base
+from sqlalchemy import Column, String, Integer, Float, DateTime, ForeignKey, Text, create_engine
+
+
+class User(Base):
+    __tablename__ = 'users'
+
+    userId = Column(Integer, primary_key=True, autoincrement=True, nullable=False)
+    userName = Column(String(100), nullable=False, unique=True)
+    email = Column(String(250), nullable=False, unique=True)
+
+    def __repr__(self):
+        return f"<User {self.userName}>"
+
+
+class Trip(Base):
+    __tablename__ = 'trips'
+
+    tripIdShared = Column(String(6), primary_key=True)
+    tripTitle = Column(String(150), nullable=False)
+    currencies = Column(String(500), nullable=False)
+
+    def __repr__(self):
+        return f"<Trip {self.tripTitle}>"
+
+
+class Expense(Base):
+    __tablename__ = 'expenses'
+
+    expenseId = Column(Integer, primary_key=True, autoincrement=True)
+    expenseDate = Column(DateTime, nullable=False, default=datetime.utcnow)
+    expenseDesc = Column(String(350), nullable=False)
+    expenseAmount = Column(Float, nullable=False)
+    expensePaidBy = Column(Integer, ForeignKey('users.userId', ondelete='CASCADE'), nullable=False)
+    expenseSplitBw = Column(String(2000), nullable=False)
+    tripId = Column(String(6), ForeignKey('trips.tripIdShared'), nullable=False)
+
+    def __repr__(self):
+        return f"<Expense {self.expenseDesc}>"
+
+
+class Balance(Base):
+    __tablename__ = 'Balance'
+
+    tripId = Column(String(6), ForeignKey('trips.tripIdShared'), primary_key=True)
+    userId = Column(Integer, ForeignKey('users.userId'), primary_key=True)
+    expenseId = Column(Integer, ForeignKey('expenses.expenseId'), primary_key=True)
+    amount = Column(Float, nullable=False)
+    borrowedFrom = Column(Integer, ForeignKey('users.userId'), nullable=True)
+
+    def __repr__(self):
+        return f"<Balance tripId={self.tripId} userId={self.userId} expenseId={self.expenseId}>"
+
+
+class TripRequest(Base):
+    __tablename__ = 'tripRequest'
+
+    tripId = Column(String(6), ForeignKey('trips.tripIdShared'), primary_key=True)
+    userId = Column(Integer, ForeignKey('users.userId'), primary_key=True)
+
+    def __repr__(self):
+        return f"<TripRequest tripId={self.tripId} userId={self.userId}>"
+
+
+class UserTrip(Base):
+    __tablename__ = 'userTrips'
+
+    userId = Column(Integer, ForeignKey('users.userId'), primary_key=True)
+    tripId = Column(String(6), ForeignKey('trips.tripIdShared'), primary_key=True)
+
+    def __repr__(self):
+        return f"<UserTrip userId={self.userId} tripId={self.tripId}>"
